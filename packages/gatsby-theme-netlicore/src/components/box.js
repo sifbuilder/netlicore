76c1729fa4a8a05038fbed0222c634b859b232a6
sifbuilder <sifbuilder@gmail.com>
2019-06-05 20:47:45 +0200

    Init_git

---
+++ packages/gatsby-theme-netlicore/src/components/box.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ({ children }) => (
+  <div
+    style={{
+      padding: '1rem',
+      backgroundColor: 'tomato',
+      }}
+  >
+    {children}
+  </div>
+)
